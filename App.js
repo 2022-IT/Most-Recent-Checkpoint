892402cae506bd657a0fc193ae58fe4c294add8d
2022-IT <116741046+2022-IT@users.noreply.github.com>
2022-12-23 16:38:37 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import HideShowDiv from './HideshowDiv'
+
+function App() {  
+     
+  return (  
+    <div className="App">  
+      <HideShowDiv />  
+    </div>  
+  );  
+}  
+ 
+export default App;
